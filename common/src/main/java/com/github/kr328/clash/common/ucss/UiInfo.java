01ba485a7363371da399c522edf6f989a8ceee42
宝尔 <shangji.sj@alibaba-inc.com>
2021-07-17 20:43:18 +0800

    修改

---
+++ common/src/main/java/com/github/kr328/clash/common/ucss/UiInfo.java
@@ -0,0 +1,12 @@
+package com.github.kr328.clash.common.ucss;
+
+/**
+ * @author shangji_cd
+ */
+public class UiInfo {
+    public boolean isTablet;
+    public boolean switchAccount;
+    public boolean switchMode;
+    public boolean needPatchNode;
+    public String currentNode;
+}
